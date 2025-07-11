7929d06fc8effb25bb84958a9235a31409ffa987
cpuniversegithub <141985360+cpuniversegithub@users.noreply.github.com>
2025-07-11 15:17:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Smooth Scroll Function
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Toggle Mobile Navbar
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
